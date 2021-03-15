767a99666c1bb61693d60d7f459e5f24469bf222
mexell96 <mexell96@gmail.com>
2021-03-15 15:24:51 +0300

    added Pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Blog from "./pages/Blog/Blog";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import Pages from "./pages/Pages/Pages";
+import Shop from "./pages/Shop/Shop";
 
 function App() {
-  return <div>cghchHello world!</div>;
+  return (
+    <div>
+      <Home />
+      <Shop />
+      <Blog />
+      <Pages />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
