c03885c4c459e2d63b9eff3982f0885ee068c9b5
mexell96 <mexell96@gmail.com>
2021-03-16 17:30:21 +0300

    changed styles for Promo

--- src/pages/Home/sections/Promo.jsx
+++ src/pages/Home/sections/Promo.jsx
@@ -9,41 +9,48 @@ import sales from "../../../images/Home/Promo/c5.jpg";
 const Promo = () => {
   return (
     <div className={classes.main}>
-      <div className={classes.pic}>
-        <div>
-          <img
-            src={pic1}
-            alt="pic1"
-            className={classes.imgStyle}
-            style={{ "max-width": "100%", height: "auto" }}
-          />
-          <img
-            src={pic2}
-            alt="pic2"
-            className={classes.imgStyle}
-            style={{ "max-width": "100%", height: "auto" }}
-          />
+      <div>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div className={classes.darken}>
+            <div>
+              <img
+                src={pic1}
+                alt="pic1"
+                style={{ "max-width": "100%", height: "auto" }}
+              />
+            </div>
+          </div>
+          <div className={classes.darken} style={{ width: "245px" }}>
+            <img
+              src={pic2}
+              alt="pic2"
+              style={{ "max-width": "100%", height: "auto" }}
+            />
+          </div>
         </div>
-        <div>
-          <img
-            src={pic3}
-            alt="pic3"
-            className={classes.imgStyle}
-            style={{ "max-width": "100%", height: "auto" }}
-          />
-          <img
-            src={pic4}
-            alt="pic4"
-            className={classes.imgStyle}
-            style={{ "max-width": "100%", height: "auto" }}
-          />
+
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div className={classes.darken} style={{ width: "245px" }}>
+            <img
+              src={pic3}
+              alt="pic3"
+              style={{ "max-width": "100%", height: "auto" }}
+            />
+          </div>
+          <div className={classes.darken}>
+            <img
+              src={pic4}
+              alt="pic4"
+              style={{ "max-width": "100%", height: "auto" }}
+            />
+          </div>
         </div>
       </div>
-      <div>
+
+      <div className={classes.darken} style={{ height: "435px" }}>
         <img
           src={sales}
           alt="sales"
-          className={classes.imgStyle}
           style={{ "max-width": "100%", height: "auto" }}
         />
       </div>
