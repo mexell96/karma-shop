9f7609a096082b5e54552a4710bbc831472fc21a
mexell96 <mexell96@gmail.com>
2021-03-16 13:11:24 +0300

    added section Promo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Conditions from "./sections/Conditions";
 import Main from "./sections/Main";
+import Promo from "./sections/Promo";
 
 const Home = () => {
   return (
     <>
       <Main />
       <Conditions />
+      <Promo />
     </>
   );
 };
---
+++ src/pages/Home/sections/Promo.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import classes from "./Promo.module.css";
+import pic1 from "../../../images/Home/Promo/c1.jpg";
+import pic2 from "../../../images/Home/Promo/c2.jpg";
+import pic3 from "../../../images/Home/Promo/c3.jpg";
+import pic4 from "../../../images/Home/Promo/c4.jpg";
+import sales from "../../../images/Home/Promo/c5.jpg";
+
+const Promo = () => {
+  return (
+    <div className={classes.main}>
+      <div className={classes.pic}>
+        <div>
+          <img src={pic1} alt="pic1" className={classes.imgStyle} />
+          <img
+            src={pic2}
+            alt="pic2"
+            className={classes.imgStyle}
+            style={{ height: 184 }}
+          />
+        </div>
+        <div>
+          <img
+            src={pic3}
+            alt="pic3"
+            className={classes.imgStyle}
+            style={{ height: 184 }}
+          />
+          <img src={pic4} alt="pic4" className={classes.imgStyle} />
+        </div>
+      </div>
+      <div>
+        <img src={sales} alt="sales" className={classes.imgStyle} />
+      </div>
+    </div>
+  );
+};
+
+export default Promo;
