5c5861b02651678ba954aaebebb1a7aa086eccbf
mexell96 <mexell96@gmail.com>
2021-03-16 10:41:37 +0300

    added logo to navbar

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,53 +1,59 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import classes from "./Navbar.module.css";
+import logo from "../images/Navbar/logo.png";
 
 export const Navbar = () => {
   return (
     <nav className={classes.nav}>
-      <ul className={classes.ul}>
-        <li className={classes.li}>
-          <NavLink
-            exact
-            to="/"
-            className={classes.NavLink}
-            activeClassName={classes.selected}>
-            HOME
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/shop"
-            className={classes.NavLink}
-            activeClassName={classes.selected}>
-            SHOP
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/blog"
-            className={classes.NavLink}
-            activeClassName={classes.selected}>
-            BLOG
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/pages"
-            className={classes.NavLink}
-            activeClassName={classes.selected}>
-            PAGES
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/contact"
-            className={classes.NavLink}
-            activeClassName={classes.selected}>
-            CONTACT
-          </NavLink>
-        </li>
-      </ul>
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+      <div>
+        <ul className={classes.ul}>
+          <li className={classes.li}>
+            <NavLink
+              exact
+              to="/"
+              className={classes.NavLink}
+              activeClassName={classes.selected}>
+              HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/shop"
+              className={classes.NavLink}
+              activeClassName={classes.selected}>
+              SHOP
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/blog"
+              className={classes.NavLink}
+              activeClassName={classes.selected}>
+              BLOG
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/pages"
+              className={classes.NavLink}
+              activeClassName={classes.selected}>
+              PAGES
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              className={classes.NavLink}
+              activeClassName={classes.selected}>
+              CONTACT
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
