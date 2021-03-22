172a6cb0b5a65363a0c3f6197c2cbbb6fe2c8a0b
mexell96 <mexell96@gmail.com>
2021-03-22 10:57:24 +0300

    added material grid

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,15 +3,18 @@ import Conditions from "./sections/Conditions";
 import Main from "./sections/Main";
 import Products from "./sections/Products";
 import Promo from "./sections/Promo";
+import Grid from "@material-ui/core/Grid";
 
 const Home = () => {
   return (
-    <>
-      <Main />
-      <Conditions />
-      <Promo />
-      <Products />
-    </>
+    <Grid container justify="center">
+      <Grid item xl={10} lg={10} md={10} m={10} xs={10}>
+        <Main />
+        <Conditions />
+        <Promo />
+        <Products />
+      </Grid>
+    </Grid>
   );
 };
 
--- src/pages/Home/sections/Conditions.jsx
+++ src/pages/Home/sections/Conditions.jsx
@@ -1,34 +1,65 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import freeDelivery from "../../../images/Home/Conditions/f-icon1.png";
 import returnPolicy from "../../../images/Home/Conditions/f-icon2.png";
 import support from "../../../images/Home/Conditions/f-icon3.png";
 import securePayment from "../../../images/Home/Conditions/f-icon4.png";
 import classes from "./Conditions.module.css";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(8),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
 const Conditions = () => {
+  const styles = useStyles();
+
   return (
-    <div className={classes.Conditions}>
-      <div className={classes.blocks}>
-        <img src={freeDelivery} alt="freeDelivery" className={classes.img} />
-        <h3 className={classes.h3}>Free Delivery</h3>
-        <span className={classes.span}>Free Shipping on all order</span>
-      </div>
-      <div className={classes.blocks}>
-        <img src={returnPolicy} alt="returnPolicy" className={classes.img} />
-        <h3 className={classes.h3}>Return Policy</h3>
-        <span className={classes.span}>Free Shipping on all order</span>
-      </div>
-      <div className={classes.blocks}>
-        <img src={support} alt="support" className={classes.img} />
-        <h3 className={classes.h3}>24/7 Support</h3>
-        <span className={classes.span}>Free Shipping on all order</span>
-      </div>
-      <div className={classes.blocks}>
-        <img src={securePayment} alt="securePayment" className={classes.img} />
-        <h3 className={classes.h3}>Secure Payment</h3>
-        <span className={classes.span}>Free Shipping on all order</span>
-      </div>
-    </div>
+    <Grid container spacing={5} justify="center">
+      <Grid item xl>
+        <Paper className={styles.paper}>
+          <img src={freeDelivery} alt="freeDelivery" className={classes.img} />
+          <h3 className={classes.h3}>Free Delivery</h3>
+          <span className={classes.span}>Free Shipping on all order</span>
+        </Paper>
+      </Grid>
+
+      <Grid item xl>
+        <Paper className={styles.paper}>
+          <img src={returnPolicy} alt="returnPolicy" className={classes.img} />
+          <h3 className={classes.h3}>Return Policy</h3>
+          <span className={classes.span}>Free Shipping on all order</span>
+        </Paper>
+      </Grid>
+
+      <Grid item xl>
+        <Paper className={styles.paper}>
+          <img src={support} alt="support" className={classes.img} />
+          <h3 className={classes.h3}>24/7 Support</h3>
+          <span className={classes.span}>Free Shipping on all order</span>
+        </Paper>
+      </Grid>
+
+      <Grid item xl>
+        <Paper className={styles.paper}>
+          <img
+            src={securePayment}
+            alt="securePayment"
+            className={classes.img}
+          />
+          <h3 className={classes.h3}>Secure Payment</h3>
+          <span className={classes.span}>Free Shipping on all order</span>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 };
 
